2dc96cb1d6493cc12b2473ed06ef4b9fe5186799
Eduardo Mejia <edumejia30@gmail.com>
2018-10-15 11:35:19 -0400

    Correcciones

--- src/app/Controllers/Api/PagosttController.php
+++ src/app/Controllers/Api/PagosttController.php
@@ -46,7 +46,7 @@ class PagosttController extends BaseController {
     }
 
     public function getSuccessfulPayment($payment_code, $transaction_id = NULL){
-        \Log::info('Successful transaction: '.$payment_code.' | '.$external_payment_code.' | '.json_encode(request()->all()));
+        \Log::info('Successful transaction: '.$payment_code.' | '.$transaction_id.' | '.json_encode(request()->all()));
         if($payment_code&&request()->has('transaction_id')){
             $api_transaction = false;
             if($transaction_id&&$ptt_transaction = \Solunes\Pagostt\App\PttTransaction::where('payment_code',$payment_code)->where('transaction_id',$transaction_id)->where('status','holding')->first()){
